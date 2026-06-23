b47eb6ce8f807b388b48ea8ca85146c66b34f0dd
SoorajPandey7 <237211271+cybersonic0402-dot@users.noreply.github.com>
2026-06-23 16:16:40 +0530

    set customer cookie

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,38 +1,43 @@
-import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
-
   const code = url.searchParams.get("code");
 
-  return new Response(
-    `
-    <html>
-      <body style="font-family:Arial;padding:40px">
-        <h1>LOGIN SUCCESS</h1>
-
-        <h3>Authorization Code</h3>
-        <pre>${code}</pre>
-
-        <button onclick="getToken()">Get Token</button>
-
-        <pre id="result"></pre>
-
-        <script>
-          async function getToken() {
-            const res = await fetch('/api/auth/token?code=${code}');
-            const data = await res.text();
-
-            document.getElementById('result').innerText = data;
-          }
-        </script>
-      </body>
-    </html>
-    `,
+  const tokenResponse = await fetch(
+    "https://shopify.com/authentication/100596318496/oauth/token",
     {
+      method: "POST",
       headers: {
-        "Content-Type": "text/html",
+        "Content-Type": "application/x-www-form-urlencoded",
       },
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        client_id: "777015f3-c7ba-435b-a6c9-e95c84bd5953",
+        code: code || "",
+        redirect_uri:
+          "https://hotworx-login.vercel.app/api/auth/callback",
+      }),
     }
   );
+
+  const data = await tokenResponse.json();
+
+  const response = NextResponse.redirect(
+    "https://test-next-day-nutra-hotworx.myshopify.com/"
+  );
+
+  response.cookies.set(
+    "customer_access_token",
+    data.access_token,
+    {
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60,
+    }
+  );
+
+  return response;
 }
