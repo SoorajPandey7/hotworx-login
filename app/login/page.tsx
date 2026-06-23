aa10a765bffe1f882b2efa431095e7d02e6e4f2d
SoorajPandey7 <237211271+cybersonic0402-dot@users.noreply.github.com>
2026-06-23 11:25:39 +0530

    fix login url

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,10 @@
 export default function LoginPage() {
-  const clientId = "88420f56-41b8-419b-8169-ba8f9fba8371";
-
   const loginUrl =
     "https://shopify.com/authentication/100596318496/oauth/authorize" +
-    `?client_id=${clientId}` +
-    `&redirect_uri=https://hotworx-login.vercel.app/api/auth/callback` +
-    `&response_type=code`;
+    "?client_id=88420f56-41b8-419b-8169-ba8f9fba8371" +
+    "&redirect_uri=https%3A%2F%2Fhotworx-login.vercel.app%2Fapi%2Fauth%2Fcallback" +
+    "&response_type=code" +
+    "&scope=openid+email";
 
   return (
     <div style={{ padding: "50px" }}>
