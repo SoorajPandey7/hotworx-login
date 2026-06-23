77a8a4af6a0c9e52c7c4451bb4005184bc07133a
SoorajPandey7 <237211271+cybersonic0402-dot@users.noreply.github.com>
2026-06-23 12:16:40 +0530

    callback new code

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -2,15 +2,11 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
-
   const code = url.searchParams.get("code");
 
-  return new Response(
-    `LOGIN SUCCESS<br><br>CODE: ${code}`,
-    {
-      headers: {
-        "Content-Type": "text/html",
-      },
-    }
+  console.log("Shopify Code:", code);
+
+  return NextResponse.redirect(
+    new URL("/", "https://hotworx-login.vercel.app")
   );
 }
