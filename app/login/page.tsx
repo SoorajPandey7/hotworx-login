76c521adc397adc41cc06bc5a4f73706150ed3f6
SoorajPandey7 <237211271+cybersonic0402-dot@users.noreply.github.com>
2026-06-23 11:45:18 +0530

    fix shopify oauth url

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,16 @@
 export default function LoginPage() {
+  const loginUrl =
+    "https://shopify.com/authentication/100596318496/oauth/authorize" +
+    "?client_id=88420f56-41b8-419b-8169-ba8f9fba8371" +
+    "&redirect_uri=https%3A%2F%2Fhotworx-login.vercel.app%2Fapi%2Fauth%2Fcallback" +
+    "&response_type=code" +
+    "&scope=openid%20email";
+
   return (
     <div style={{ padding: "50px" }}>
       <h1>Login</h1>
 
-      <a href="https://shopify.com/authentication/100596318496/login">
+      <a href={loginUrl}>
         <button>Login with Shopify</button>
       </a>
     </div>
