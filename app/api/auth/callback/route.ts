0435dd46c7fc1f02d5ad815860a5e2e23aa48f0c
SoorajPandey7 <237211271+cybersonic0402-dot@users.noreply.github.com>
2026-06-23 12:03:27 +0530

    debug callback

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,7 +1,16 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.redirect(
-    new URL("/", "https://hotworx-login.vercel.app")
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+
+  const code = url.searchParams.get("code");
+
+  return new Response(
+    `LOGIN SUCCESS<br><br>CODE: ${code}`,
+    {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    }
   );
 }
