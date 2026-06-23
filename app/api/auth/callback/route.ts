4aab3d72989b921493fd93aa6a29599ee8dd7b4a
SoorajPandey7 <237211271+cybersonic0402-dot@users.noreply.github.com>
2026-06-23 14:12:46 +0530

    redirect to homepage after login delay set

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,12 +1,20 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const code = url.searchParams.get("code");
-
-  console.log("Shopify Code:", code);
-
-  return NextResponse.redirect(
-    new URL("https://test-next-day-nutra-hotworx.myshopify.com/")
-  );
+  return new Response(`
+    <html>
+      <body>
+        <script>
+          setTimeout(() => {
+            window.location.href = "https://test-next-day-nutra-hotworx.myshopify.com/";
+          }, 2000);
+        </script>
+        Logging in...
+      </body>
+    </html>
+  `, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
